1716eac67c108e1d3ed824a89666a2fded6505cc
JuanEstebanEraso <123413910+JuanEstebanEraso@users.noreply.github.com>
2026-05-19 10:40:20 -0500

    feat(infra): add local dev databases terraform configuration with kubernetes remote state backend (#2)
    * feat(infra): add local dev databases terraform configuration
    
    * feat(infra): configure kubernetes remote state backend for terraform

---
+++ terraform/namespaces.tf
@@ -0,0 +1,17 @@
+resource "kubernetes_namespace" "dev" {
+  metadata {
+    name = "dev"
+  }
+}
+
+resource "kubernetes_namespace" "stage" {
+  metadata {
+    name = "stage"
+  }
+}
+
+resource "kubernetes_namespace" "prod" {
+  metadata {
+    name = "prod"
+  }
+}
