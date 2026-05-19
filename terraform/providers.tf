114915ceedeb74eda0a84406e356b899cc018310
JuanEstebanEraso <juaneseragut11@gmail.com>
2026-05-18 21:09:42 -0500

    feat(infra): add local dev databases terraform configuration

---
+++ terraform/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Configuración de Kubernetes para que funcione automáticamente en Windows y Linux
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
